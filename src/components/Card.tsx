c49dfcfef6e83d4e4f41508a92a9926360eba128
minhivan <minhpn.work@gmail.com>
2024-03-08 00:23:44 +0700

    update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,96 +5,96 @@ import { useDrag } from 'react-use-gesture'
 import styles from '../styles.module.css'
 
 const cards = [
-  '/images/1.jpg',
-  '/images/2.jpg',
-  '/images/3.jpg',
-  '/images/4.jpg',
-  '/images/5.jpg',
-  '/images/6.jpg',
-  '/images/7.jpg',
-  '/images/8.jpg',
-  '/images/9.jpg',
-  '/images/10.jpg',
-  '/images/11.jpg',
-  '/images/12.jpg',
-  '/images/13.jpg',
-  '/images/14.jpg',
-  '/images/16.jpg',
-  '/images/17.jpg',
-  '/images/18.jpg',
-  '/images/19.jpg',
-  '/images/20.jpg',
-  '/images/21.jpg',
+    '/images/21.jpg',
+    '/images/20.jpg',
+    '/images/19.jpg',
+    '/images/18.jpg',
+    '/images/17.jpg',
+    '/images/16.jpg',
+    '/images/14.jpg',
+    '/images/13.jpg',
+    '/images/12.jpg',
+    '/images/11.jpg',
+    '/images/10.jpg',
+    '/images/9.jpg',
+    '/images/8.jpg',
+    '/images/7.jpg',
+    '/images/6.jpg',
+    '/images/5.jpg',
+    '/images/4.jpg',
+    '/images/3.jpg',
+    '/images/2.jpg',
+    '/images/1.jpg',
 ]
 
 // These two are just helpers, they curate spring data, values that are later being interpolated into css
 const to = (i: number) => ({
-  x: 0,
-  y: i * -2,
-  scale: 1,
-  rot: -10 + Math.random() * 20,
-  delay: i * 100,
+    x: 0,
+    y: i * -2,
+    scale: 1,
+    rot: -10 + Math.random() * 20,
+    delay: i * 100,
 })
 const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 // This is being used down there in the view, it interpolates rotation and scale into a css transform
 const trans = (r: number, s: number) =>
-  `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
+    `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
 function Deck() {
-  const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
-  const [props, api] = useSprings(cards.length, i => ({
-    ...to(i),
-    from: from(i),
-  })) // Create a bunch of springs using the helpers above
-  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
-  const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
-    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
-    api.start(i => {
-      if (index !== i) return // We're only interested in changing spring-data for the current spring
-      const isGone = gone.has(index)
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
-      const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
-      const scale = down ? 1.2 : 1 // Active cards lift up a bit
-      return {
-        x,
-        rot,
-        scale,
-        delay: undefined,
-        config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
-      }
+    const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
+    const [props, api] = useSprings(cards.length, i => ({
+        ...to(i),
+        from: from(i),
+    })) // Create a bunch of springs using the helpers above
+    // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+    const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
+        const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+        const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+        if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+        api.start(i => {
+            if (index !== i) return // We're only interested in changing spring-data for the current spring
+            const isGone = gone.has(index)
+            const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+            const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+            const scale = down ? 1.2 : 1 // Active cards lift up a bit
+            return {
+                x,
+                rot,
+                scale,
+                delay: undefined,
+                config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 },
+            }
+        })
+        if (!down && gone.size === cards.length)
+            setTimeout(() => {
+                gone.clear()
+                api.start(i => to(i))
+            }, 600)
     })
-    if (!down && gone.size === cards.length)
-      setTimeout(() => {
-        gone.clear()
-        api.start(i => to(i))
-      }, 600)
-  })
-  // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
-  return (
-    <>
-      {props.map(({ x, y, rot, scale }, i) => (
-        <animated.div className={styles.deck} key={i} style={{ x, y }}>
-          {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
-          <animated.div
-            {...bind(i)}
-            style={{
-              transform: interpolate([rot, scale], trans),
-              backgroundImage: `url(${cards[i]})`,
-            }}
-          />
-        </animated.div>
-      ))}
-    </>
-  )
+    // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+    return (
+        <>
+            {props.map(({ x, y, rot, scale }, i) => (
+                <animated.div className={styles.deck} key={i} style={{ x, y }}>
+                    {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+                    <animated.div
+                        {...bind(i)}
+                        style={{
+                            transform: interpolate([rot, scale], trans),
+                            backgroundImage: `url(${cards[i]})`,
+                        }}
+                    />
+                </animated.div>
+            ))}
+        </>
+    )
 }
 
 export default function Card() {
-  return (
+    return (
 
-    <div className={styles.container}>
-      <Deck />
-    </div>
-  )
+        <div className={styles.container}>
+            <Deck />
+        </div>
+    )
 }
