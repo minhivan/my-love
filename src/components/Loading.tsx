4412cfcd04f62b9682347f8103fe2c95d17f6552
Minh.N.Pham <minhpn.work@gmail.com>
2025-02-15 11:45:24 +0700

    [update] update neon

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -15,22 +15,21 @@ export default function Loading() {
       height: 0,
       innerHeight: 0,
       transform: 'perspective(600px) rotateX(0deg)',
-      color: '#8fa5b6',
-    },
+      },
     enter: [
       { opacity: 1, height: 80, innerHeight: 80 },
-      { transform: 'perspective(600px) rotateX(180deg)', color: '#28d79f' },
+      { transform: 'perspective(600px) rotateX(180deg)' },
       { transform: 'perspective(600px) rotateX(0deg)' },
     ],
-    leave: [{ color: '#c23369' }, { innerHeight: 0 }, { opacity: 0, height: 0 }],
-    update: { color: '#28b4d7' },
+    leave: [{ innerHeight: 0 }, { opacity: 0, height: 0 }],
+    update: { },
   })
 
   const reset = useCallback(() => {
     ref.current.forEach(clearTimeout)
     ref.current = []
     set([])
-    ref.current.push(setTimeout(() => set(['Chúng tôi', 'Yêu nhau', 'Như thế nào']), 500))
+    ref.current.push(setTimeout(() => set(['Chúng tôi', 'yêu nhau', 'như thế nào']), 700))
     // ref.current.push(setTimeout(() => set(['Chúng tôi', 'Kiwis']), 5000))
     // ref.current.push(setTimeout(() => set(['Chúng tôi', 'Bananas', 'Kiwis']), 8000))
   }, [])
@@ -52,7 +51,7 @@ export default function Loading() {
         { card ? <Card /> : (<div className={styles.main}>
         {transitions(({ innerHeight, ...rest }, item) => (
           <animated.div className={styles.transitionsItem} style={rest} onClick={reset}>
-            <animated.div style={{ overflow: 'hidden', height: innerHeight }}>{item}</animated.div>
+            <animated.div style={{  height: innerHeight }}>{item}</animated.div>
           </animated.div>
         ))}
       </div>)}
